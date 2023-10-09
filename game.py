1912c7b9365855fd3b3f588016c9bd6b04e4a28f
Cejpek0 <c.michal.70@gmail.com>
2023-10-09 16:18:14 +0200

    init

---
+++ game.py
@@ -0,0 +1,22 @@
+import pygame
+from enum import Enum
+
+pygame.init()
+font = pygame.font.Font('arial.ttf', 25)
+
+BLOCK_SIZE = 20
+
+
+class ScotlandYardGame:
+
+    def __init__(self, w=640, h=480):
+        self.w = w
+        self.h = h
+        # init display
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('ScotlandYard')
+        self.clock = pygame.time.Clock()
+        self.reset()
+
+    def reset(self):
+        pass
