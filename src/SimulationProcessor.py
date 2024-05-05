7a9421953b811ff88ec04063881da24c27d10884
Cejpek0 <c.michal.70@gmail.com>
2024-05-05 20:36:44 +0200

    new graphs

--- src/SimulationProcessor.py
+++ src/SimulationProcessor.py
@@ -52,7 +52,7 @@ class SimulationProcessor:
             hue='variable',
             style='variable',
             kind='line',
-            lw=2,
+            lw=2.5,
             errorbar=None,
             facet_kws=dict(legend_out=False),
             data=dataframe.melt(id_vars='train_iteration',
